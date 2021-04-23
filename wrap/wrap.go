58e1732aa7d2875279821e0b273d23397d1cde7b
Hugefiver <i@iruri.moe>
2021-04-23 19:16:34 +0800

    wrap conn

--- wrap/wrap.go
+++ wrap/wrap.go
@@ -1,1 +1,31 @@
 package wrap
+
+import (
+	"net"
+
+	"github.com/hugefiver/qush/quic"
+)
+
+type ConnWrapper struct {
+	quic.Stream
+	session quic.Session
+}
+
+func From(stream quic.Stream, session quic.Session) *ConnWrapper {
+	return &ConnWrapper{
+		Stream:  stream,
+		session: session,
+	}
+}
+
+func (c ConnWrapper) LocalAddr() net.Addr {
+	return c.session.LocalAddr()
+}
+
+func (c ConnWrapper) RemoteAddr() net.Addr {
+	return c.session.RemoteAddr()
+}
+
+func (c ConnWrapper) Session() quic.Session {
+	return c.session
+}
