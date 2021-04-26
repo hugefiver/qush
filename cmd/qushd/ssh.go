9092d3005d8968bd19515d35bf80136817f72068
Hugefiver <i@iruri.moe>
2021-04-26 13:45:21 +0800

    fix log msg

--- cmd/qushd/ssh.go
+++ cmd/qushd/ssh.go
@@ -130,9 +130,14 @@ type WinSize struct {
 }
 
 func logAuthLog(conn ssh.ConnMetadata, method string, err error) {
-	if err != nil {
-		log.Info().Err(err).Msgf("Failed to auth user %s login from %v using %s", conn.User(), conn.RemoteAddr(), method)
-	} else {
-		log.Info().Msgf("Succeed to auth login from %v using %s", conn.RemoteAddr(), method)
+	switch method {
+	case "password":
+		if err != nil {
+			log.Info().Err(err).Msgf("Failed to auth user %s login from %v using %s", conn.User(), conn.RemoteAddr(), method)
+		} else {
+			log.Info().Msgf("Succeed to auth login from %v using %s", conn.RemoteAddr(), method)
+		}
+	default:
+		return
 	}
 }
